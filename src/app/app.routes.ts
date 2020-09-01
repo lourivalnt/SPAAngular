45e05522c69cf957459daf250f2957b6435ac51a
Lourival Linard <l.linard@gmail.com>
2020-09-01 09:26:06 -0300

    DataBind na prática

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
+import { DataBindingComponent } from './demos/data-binding/data-binding.component';
 import { Routes } from '@angular/router';
 import { HomeComponent } from './navegacao/home/home.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
@@ -7,5 +8,6 @@ export const rootRouterConfig: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
     { path: 'home', component: HomeComponent},
     { path: 'contato', component: ContatoComponent },
-    { path: 'sobre', component: SobreComponent }
+    { path: 'sobre', component: SobreComponent },
+    { path: 'feature-data-binding', component: DataBindingComponent }
 ];
